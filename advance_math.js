0e60a8a58a4d1f85d4dfa6dd7a3735f59b3ad0a6
ummesabiha <umme.sabiha@mztech.us>
2021-03-11 18:01:42 +0600

    js

---
+++ advance_math.js
@@ -0,0 +1,20 @@
+// let a = 6;
+// a++; // a = a + 1
+// console.log(a);
+// let b = 5;
+// b --;
+// console.log(b);
+
+// var x;
+// var y = 9;
+// x = y++;  //x= y; y = y + 1;
+// console.log("x",x,"y",y);
+
+let x;
+let y = 9;
+// x = y;
+// y = y + 1;
+// console.log("x",x,"y",y);
+
+x = ++y; // y = y + 1;  x = y;
+console.log("x",x,"y",y);
---
+++ math.js
@@ -0,0 +1,18 @@
+let productName = "Pen"
+let price = 10;
+let quantity = 5;
+console.log(productName + price * quantity);
+
+// // area calculate
+
+let radius =9;
+const pi = 3.14159;
+let areaOfCircle = pi * (radius*radius);
+console.log("Area of the circle is", areaOfCircle);
+
+//percentage
+
+var pencil = 5;
+var pen = 5%2;
+console.log(pen);
+
