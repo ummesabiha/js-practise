f742ff66bd9a06afb0e6e9d0b2666e9d5cc37b0f
ummesabiha <umme.sabiha@mztech.us>
2021-03-11 19:24:43 +0600

    js

---
+++ comparison.js
@@ -0,0 +1,32 @@
+let a = 7;
+if (a < 10) {
+    console.log(a)
+    console.log("This number", a , "is less than", 10);
+} else { 
+    console.log("This number", a , "is greater than", 10);
+}
+
+let x = 8;
+if (x < 10) {
+    console.log(x)
+    console.log("This number", x , "is greater than", 10);
+} else if (x < 20) {
+    console.log("This number", x , "is less than", 20);
+} else { 
+    console.log("This number", x , "is less than", 10);
+}
+ 
+var y = 20;
+var z = 21;
+if (y>z) {
+    console.log("y is smaller than z");
+} else {
+    console.log("y is greater than z");
+}
+
+let r = 8;
+if (8 == r) {
+    console.log("n is not equal to 8");
+} else {
+    console.log("n is equal to 8");
+}
