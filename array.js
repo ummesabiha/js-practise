727809e4745c0db71620b968fefe863330e082f5
ummesabiha <umme.sabiha@mztech.us>
2021-03-28 16:15:43 +0600

    js

--- array.js
+++ array.js
@@ -7,3 +7,19 @@ let lastElement = myList.pop(); //return back last removed  element
 
 console.log(myList);
 console.log(lastElement);
+
+let fruits = ["apple", "orange", "banana"];
+let flowers = ["lily", "freesia", "peruvian lily"];
+
+// let lastFruits = fruits.pop();
+// flowers.push(lastFruits)
+
+// let fruitsToRemove = fruits[1];
+// flowers.push(fruitsToRemove);
+
+// delete(fruits[1]);
+fruits.splice(1,0);
+
+
+console.log(fruits, flowers);
+console.log(fruits.length, flowers.length);
