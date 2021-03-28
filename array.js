1a1b63a3e3890869c05a9b2d540e2ef05f8ca460
ummesabiha <umme.sabiha@mztech.us>
2021-03-28 16:55:31 +0600

    js

--- array.js
+++ array.js
@@ -20,6 +20,23 @@ let flowers = ["lily", "freesia", "peruvian lily"];
 // delete(fruits[1]);
 fruits.splice(1,0);
 
-
 console.log(fruits, flowers);
 console.log(fruits.length, flowers.length);
+
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September"];
+let part =  month.slice(1,4);
+
+console.log(month,part);
+
+let day  = [
+            "Saturday", // 0 = -7
+            "Sunday", // 1 = -6
+            "Monday", // 2 = -5
+            "Tuesday", // 3 = -4
+            "Wednesday", // 4 -3
+            "Thursday", // 5 = -2
+            "Friday" // 6 = -1
+                    ];
+let list = day.slice(-2); //slice from this
+
+console.log(day,list);
